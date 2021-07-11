4de86cce0dcf022d5dc90fc6100657a3779668c4
Sergey B <sergeyvbo@outlook.com>
2021-07-11 22:30:10 +0300

    FlappyBird - скопирован код

---
+++ Client/Games/FlappyBird/Models/BirdModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BlazorGames.Client.Games.FlappyBird.Models
+{
+    public class BirdModel
+    {
+        private const int MAX_HEIGHT = 500;
+        public int DistanceFromGround { get; private set; } = 100;
+        public int JumpStrength { get; private set; } = 50;
+
+        public bool IsOnGround() => DistanceFromGround <= 0;
+
+        public void Fall(int gravity)
+        {
+            DistanceFromGround -= Math.Min(gravity, DistanceFromGround);
+        }
+
+        public void Jump()
+        {
+            if (DistanceFromGround <= MAX_HEIGHT)
+            {
+                DistanceFromGround += JumpStrength;
+            }
+            
+        }
+    }
+}
---
+++ Client/Games/FlappyBird/Models/GameManager.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorGames.Client.Games.FlappyBird.Models
+{
+    public class GameManager
+    {
+        public const int GRAVITY = 2;
+
+        public event EventHandler MainLoopCompleted;
+
+        public BirdModel Bird { get; private set; }
+        public List<PipeModel> Pipes { get; private set; }
+
+        public bool IsRunning { get; private set; } = false;
+
+        public GameManager()
+        {
+            Bird = new();
+            Pipes = new();
+        }
+
+        public async void MainLoop()
+        {
+            IsRunning = true;
+            while (IsRunning)
+            {
+                MoveObjects();
+                CheckForCollisions();
+                ManagePipes();
+
+                MainLoopCompleted?.Invoke(this, EventArgs.Empty);
+                await Task.Delay(20);
+            }
+        }
+
+        public void StartGame()
+        {
+            if (IsRunning)
+            {
+                return;
+            }
+            Bird = new();
+            Pipes = new();
+            MainLoop();
+        }
+
+        public void Jump()
+        {
+            if (!IsRunning)
+            {
+                return;
+            }
+
+            Bird.Jump();
+        }
+
+        void CheckForCollisions()
+        {
+            if (Bird.IsOnGround())
+            {
+                GameOver();
+            }
+
+            var centeredPipe = Pipes.FirstOrDefault(pipe => pipe.IsCentered());
+
+            if (centeredPipe is not null)
+            {
+                bool hasCollidedWithBottom = Bird.DistanceFromGround < centeredPipe.GapBottom - 150;
+                bool hasCollidedWithTop = Bird.DistanceFromGround + 45 > centeredPipe.GapTop - 150;
+
+                if (hasCollidedWithBottom || hasCollidedWithTop)
+                {
+                    GameOver();
+                }
+            }
+        }
+        void ManagePipes()
+        {
+            if (!Pipes.Any())
+            {
+                Pipes.Add(new PipeModel());
+            }
+
+            if (Pipes.Last().DistanceFromLeft <=250)
+            {
+                Pipes.Add(new PipeModel());
+            }
+
+            if (Pipes.First().IsOffScreen())
+            {
+                Pipes.Remove(Pipes.First());
+            }
+        }
+
+        void MoveObjects()
+        {
+            Bird.Fall(GRAVITY);
+            Pipes.ForEach(pipe => pipe.Move());
+        }
+
+
+        void GameOver()
+        {
+            IsRunning = false;
+        }
+    }
+}
